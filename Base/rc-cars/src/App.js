4265028c9634aaeb7deb2fd4e0522ce6b1ecfb27
Leandro Lopes <lopesleandro.it@gmail.com>
2023-08-01 03:18:51 -0300

    feat: :sparkles: Init project React

---
+++ Base/rc-cars/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <h2>This is my first React component</h2>
+    </div>
+  );
+}
+
+export default App;
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # learn-react-springboot
+
+https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-ubuntu-20-04
